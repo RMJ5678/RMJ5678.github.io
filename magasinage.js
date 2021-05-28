ab3e1d03aa0c6543506722b5b8665c8ae754362d
RMJ5678 <83415462+RMJ5678@users.noreply.github.com>
2021-05-27 21:08:21 -0400

    Update magasinage.js

--- magasinage.js
+++ magasinage.js
@@ -1,27 +1,35 @@
-//----------------------------------------------
-alert("Tu va au magasin pour acheter un item.")
+function calcule() {
+  var prix = parseFloat(document.getElementById("priceOfItem").value);
+  var Qt = parseInt(document.getElementById("quantity").value);
+  var affiche1 = document.getElementById("affiche1");
+  var affiche2 = document.getElementById("affiche2");
+  var affiche3 = document.getElementById("affiche3");
+  var affiche4 = document.getElementById("affiche4");
  
-var prix = parseFloat(prompt("Le prix de l'item est _.__$"))
-var Qt = parseInt(prompt("Combien de cette item?"))
+  var tax = (0)
+  function calculeTax(){
+   tax = ((prix * Qt) * 0.13)
+   tax = parseFloat(tax.toFixed(2))
+   return tax
+  }
  
-var tax = (0)
-function calculeTax(){
- tax = ((prix * Qt) * 0.13)
- tax = parseFloat(tax.toFixed(2))
- return tax
+  var cout = (0)
+  function calculeCout(){
+   cout = (prix * Qt + tax)
+   cout = parseFloat(cout.toFixed(2))
+   return cout
+  }
+  
+  calculeTax()
+  calculeCout()
+
+  message1 = "Prix par item: " + prix.toFixed(2) + "$"
+  message2 = "Quantite: " + Qt
+  message3 = "Tax total: " + tax.toFixed(2) + "$" 
+  message4 = "Prix total: " + cout.toFixed(2) + "$"
+
+  affiche1.innerHTML = message1
+  affiche2.innerHTML = message2
+  affiche3.innerHTML = message3
+  affiche4.innerHTML = message4
 }
- 
-var cout = (0)
-function calculeCout(){
- cout = (prix * Qt + tax)
- cout = parseFloat(cout.toFixed(2))
- return cout
-}
- 
-//commande
-console.log("Prix par item: " + prix + "$")
-console.log("Quantité: " + Qt)
-calculeTax()
-console.log("Tax total: " + tax + "$")
-calculeCout()
-console.log("Prix total: " + cout + "$")
